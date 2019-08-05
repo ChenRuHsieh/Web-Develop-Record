566e3bdffc278180f7c16115aab3756627ee4cdd
ChenRuHsieh <peter105060016@gapp.nthu.edu.tw>
2019-08-05 21:40:37 +0800

    npm install

--- redux-weathermood-post-master/src/index.jsx
+++ redux-weathermood-post-master/src/index.jsx
@@ -5,9 +5,21 @@ import Main from 'components/Main.jsx';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
+import {createStore} from 'redux';
+import {setWeather, weather} from 'states/playground';
+
 window.onload = function() {
     ReactDOM.render(
         <Main />,
         document.getElementById('root')
     );
+
+    const store = createStore(weather);
+
+    store.subscribe(() => {
+        console.log(store.getState());
+    })
+
+    store.dispatch(setWeather(800,21));
+    store.dispatch(setWeather(800,23));
 };
